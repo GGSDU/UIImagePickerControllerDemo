dccf0291b2d2af8e676b095ee5d68e143efac6a8
Story5 <337995545@qq.com>
2017-04-05 16:00:14 +0800

    init with single view application

---
+++ UIImagePickerControllerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIImagePickerControllerDemo
+//
+//  Created by Story5 on 4/5/17.
+//  Copyright © 2017 Story5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
