5cffd7bf95776f30f9147e19717027fe1bf9aec6
Story5 <337995545@qq.com>
2017-04-05 16:54:49 +0800

    study by follow the API Reference

--- UIImagePickerControllerDemo/ViewController.m
+++ UIImagePickerControllerDemo/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIImagePickerControllerDelegate,UINavigationControllerDelegate>
+
+@property (nonatomic,strong) UIImagePickerController *picker;
 
 @end
 
@@ -24,6 +26,54 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)openPhoto:(id)sender {
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]) {
+        self.picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        [self presentViewController:self.picker animated:true completion:nil];
+    }
+}
 
+- (IBAction)openCamera:(id)sender {
+    // 1.Verify that the device is capable of picking content from the desired source. Do this by calling the isSourceTypeAvailable: class method, providing a constant from the UIImagePickerControllerSourceType enumeration.
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        self.picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        // 2.Check which media types are available for the source type you’re using, by calling the availableMediaTypesForSourceType: class method. This lets you distinguish between a camera that can be used for video recording and one that can be used only for still images.
+        NSArray *array = [UIImagePickerController availableMediaTypesForSourceType:UIImagePickerControllerSourceTypeCamera];
+        // 3.Tell the image picker controller to adjust the UI according to the media types you want to make available—still images, movies, or both—by setting the mediaTypes property.
+        self.picker.mediaTypes = @[UIImagePickerControllerOriginalImage];
+        // 4.Present the user interface. On iPhone or iPod touch, do this modally (full-screen) by calling the presentViewController:animated:completion: method of the currently active view controller, passing your configured image picker controller as the new view controller.
+        [self presentViewController:self.picker animated:true completion:nil];
+        // 5.When the user taps a button to pick a newly-captured or saved image or movie, or cancels the operation, dismiss the image picker using your delegate object. For newly-captured media, your delegate can then save it to the Camera Roll on the device. For previously-saved media, your delegate can then use the image data according to the purpose of your app.
+    }
+    
+    
+}
+
+#pragma mark - UIImagePickerControllerDelegate
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [picker dismissViewControllerAnimated:true completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info
+{
+    [picker dismissViewControllerAnimated:true completion:nil];
+}
+
+
+#pragma mark - getter
+- (UIImagePickerController *)picker
+{
+    if (_picker == nil) {
+        _picker = [[UIImagePickerController alloc] init];
+        _picker.delegate = self;
+    }
+    return _picker;
+}
+
+- (BOOL)VerifySourceType:(UIImagePickerControllerSourceType)sourceType
+{
+    return false;
+}
 
 @end
